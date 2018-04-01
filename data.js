711683661f62b1f1fed9f0824dd7a2e97ddb264c
Simon Appeltofft <31758722+VonKaffe@users.noreply.github.com>
2018-04-01 16:32:43 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+let movieData = {
+    title: "Star Wars",
+    imgUrl: "https://www.amazon.com/Star-Wars-Saga-Movie-Poster/dp/B00NH193GQ",
+    review: "The Force.",
+    actors: [
+        "Harrison Ford",
+        "Liam Nelson",
+        "Mark Hamill"
+    ]
+};
